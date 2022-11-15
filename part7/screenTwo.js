8e430dde63a43828df9922e5745d090d7385a8aa
r889s843 <ryan.strong@ku.edu>
2022-11-14 22:34:11 -0600

    Added some functionality for page 2

---
+++ part7/screenTwo.js
@@ -0,0 +1,17 @@
+//Buttons
+function gatherStone() {//adds stone resources on click
+    document.getElementById("stoneAmt").value ++;
+}
+
+
+//Village Population Managers
+let jobs = document.getElementsByClassName("jobs");
+/*jobs indexes
+i=0 gatherer
+i=1 hunter
+  */
+
+//rearragnes current popualtion to fit recent request for gatherers
+function gathererManager() {
+    
+}
