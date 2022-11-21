9547a8ad17da2bdbd2c40438cf998809215bf94b
tgspitzer <112564465+tgspitzer@users.noreply.github.com>
2022-11-20 20:00:58 -0600

    Screen1 Functionality

--- part7/screenOne.js
+++ part7/screenOne.js
@@ -1,9 +1,27 @@
 //Screen One
+function o2Tank()
+{
+    o2Btn = document.getElementById("o2tankBtn");
+    o2Btn.style.opacity = "0.5";
+    o2Btn.disabled = true; 
+    //change o2 tank amount to 1 and diable button
+    document.getElementById("o2TankAmt").value = 1; 
+}
 
-function buildCabin() {//this adds cabins and population to village
-    //call baseBtnHandler here
-    let cabinAmt = document.getElementById("cabinAmt");
-
-    cabinAmt.value ++;
-    populationChange(4);
+function collectionPod()
+{
+    collectBtn = document.getElementById("collectionBtn");
+    collectAmt = document.getElementById("collectionPodAmt").value;
+    ironAmt = document.getElementById("ironAmt").value;
+    if(collectAmt < 10)
+    {
+        //decrease iron amount by 10, increasing by 10 with each purchase
+        ironAmt = ironAmt - (10 + 10*collectAmt); 
+        collectAmt ++; 
+    }
+    else
+    {
+        collectBtn.style.opacity = "0.5";
+        collectBtn.disabled = true;
+    }
 }
