5c31448b69903e3d0fc53902283738f62fb933b3
tgspitzer <112564465+tgspitzer@users.noreply.github.com>
2022-11-20 20:02:56 -0600

    Update screenOne.js

--- part7/screenOne.js
+++ part7/screenOne.js
@@ -1,11 +1,13 @@
 //Screen One
 function o2Tank()
 {
+    ironAmt = document.getElementById("ironAmt").value;
     o2Btn = document.getElementById("o2tankBtn");
     o2Btn.style.opacity = "0.5";
     o2Btn.disabled = true; 
     //change o2 tank amount to 1 and diable button
-    document.getElementById("o2TankAmt").value = 1; 
+    document.getElementById("o2TankAmt").value = 1;
+    ironAmt = ironAmt - 30; 
 }
 
 function collectionPod()
